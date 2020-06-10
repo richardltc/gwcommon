160f07666bdd49a65082c7296be380300dc4c027
richard <richard.mace@gmail.com>
2020-06-10 14:19:10 +0100

    Updated to 0.21.5

--- gowallet-gen.go
+++ gowallet-gen.go
@@ -18,7 +18,7 @@ import (
 
 const (
 	// CAppVersion - The app version of the suite of apps
-	CAppVersion string = "0.21.4" // All of the individual apps will have the same version to make it easier for the user
+	CAppVersion string = "0.21.5" // All of the individual apps will have the same version to make it easier for the user
 	cUnknown    string = "Unknown"
 	// CDownloadURLGD - The download file lotcation for GoDivi
 	CDownloadURLGD string = "https://bitbucket.org/rmace/godivi/downloads/"
