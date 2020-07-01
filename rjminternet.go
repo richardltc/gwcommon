1576cec83b6ae51c59febb846c29350fdbe54e53
richard <richard.mace@gmail.com>
2020-07-01 07:42:53 +0100

    Added WebIsReachable to rjminternet

--- rjminternet.go
+++ rjminternet.go
@@ -2,6 +2,7 @@ package gwcommon
 
 import (
 	"fmt"
+	"net/http"
 	"os"
 	"time"
 
@@ -48,102 +49,16 @@ Loop:
 	return nil
 }
 
-// func DownloadFile(url string, dest string) {
+func WebIsReachable() bool {
+	response, err := http.Get("https://www.google.com")
 
-// 	file := path.Base(url)
+	if err != nil {
+		return false
+	}
 
-// 	log.Printf("Downloading file %s from %s\n", file, url)
+	if response.StatusCode == 200 {
+		return true
+	}
 
-// 	var path bytes.Buffer
-// 	path.WriteString(dest)
-// 	path.WriteString("/")
-// 	path.WriteString(file)
-
-// 	start := time.Now()
-
-// 	out, err := os.Create(path.String())
-
-// 	if err != nil {
-// 		fmt.Println(path.String())
-// 		panic(err)
-// 	}
-
-// 	defer out.Close()
-
-// 	headResp, err := http.Head(url)
-
-// 	if err != nil {
-// 		panic(err)
-// 	}
-
-// 	defer headResp.Body.Close()
-
-// 	size, err := strconv.Atoi(headResp.Header.Get("Content-Length"))
-
-// 	if err != nil {
-// 		panic(err)
-// 	}
-
-// 	done := make(chan int64)
-
-// 	go PrintDownloadPercent(done, path.String(), int64(size))
-
-// 	resp, err := http.Get(url)
-
-// 	if err != nil {
-// 		panic(err)
-// 	}
-
-// 	defer resp.Body.Close()
-
-// 	n, err := io.Copy(out, resp.Body)
-
-// 	if err != nil {
-// 		panic(err)
-// 	}
-
-// 	done <- n
-
-// 	elapsed := time.Since(start)
-// 	log.Printf("Download completed in %s", elapsed)
-// }
-
-// func PrintDownloadPercent(done chan int64, path string, total int64) {
-
-// 	var stop bool = false
-
-// 	for {
-// 		select {
-// 		case <-done:
-// 			stop = true
-// 		default:
-
-// 			file, err := os.Open(path)
-// 			if err != nil {
-// 				log.Fatal(err)
-// 			}
-
-// 			fi, err := file.Stat()
-// 			if err != nil {
-// 				log.Fatal(err)
-// 			}
-
-// 			size := fi.Size()
-
-// 			if size == 0 {
-// 				size = 1
-// 			}
-
-// 			var percent float64 = float64(size) / float64(total) * 100
-
-// 			fmt.Printf("%.0f", percent)
-// 			fmt.Println("%")
-// 		}
-
-// 		if stop {
-// 			break
-// 		}
-
-// 		time.Sleep(time.Second)
-// 	}
-// }
+	return false
+}
